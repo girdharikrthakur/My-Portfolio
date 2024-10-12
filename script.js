4469880f78dd430d58196d4de0a531fbbe308277
girdhari <girdharikumar101@gmail.com>
2024-10-12 17:37:00 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+const toggleButton = document.getElementsByClassName('toggle_button')[0];
+const navbarLinks = document.getElementsByClassName('navbar_links')[0];
+
+toggleButton.addEventListener('click', () => {
+ navbarLinks.classList.toggle('active');
+});
+
+
+
+
+
